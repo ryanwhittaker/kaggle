4586e3c9eb9f7d2dfe8573e49f8a6fc8a2323d06
Ryan Whittaker <ryan.w@strawhouselabs.com>
2017-05-24 21:12:05 -0700

    Fixed model. Getting 0.21 now

--- 1_boston_housing_prices/src/ols.py
+++ 1_boston_housing_prices/src/ols.py
@@ -10,7 +10,7 @@ from lib import readCSV
 data_path = os.path.dirname(os.path.abspath(__file__)) + '/../data/'
 
 def train(training_data):
-    return sm.ols(formula = 'SalePrice ~ GrLivArea + LotArea', data = training_data).fit()
+    return sm.ols(formula = 'SalePrice ~ GrLivArea + LotArea + Neighborhood', data = training_data).fit()
 
 def fillNA(data, all_data):
     return data.fillna(all_data.mean())
@@ -20,7 +20,7 @@ def run():
     real_data = readCSV(data_path + 'test.csv')
     model = train(training_data)
     print model.summary()
-    real_data['SalePrice'] = model.predict(real_data[['Id', 'GrLivArea', 'LotArea']])
+    real_data['SalePrice'] = model.predict(real_data[['Id', 'GrLivArea', 'LotArea', 'Neighborhood']])
     new_data = real_data[['Id', 'SalePrice']]
     out_path = os.path.dirname(os.path.abspath(__file__)) + '/../results/' + str(int(time.time())) + '.csv'
     new_data.to_csv(out_path, index = False, columns = ['Id', 'SalePrice'])
